dee6e09e61c6e67baf2af214ec660b66492303d9
David Lee <david@sugarcreek.xyz>
2019-06-19 21:02:28 -0400

    Serialized the input to better track the demo code snippets

--- src/faust/dev-first-table.py
+++ src/faust/dev-first-table.py
@@ -7,31 +7,29 @@ app = faust.App(
 )
 
 
-class EEGReading(faust.Record, serializer='json'):
+class EegReading(faust.Record, serializer='json'):
     time: float
     potential: float
     channel: str
 
 
-my_topic = app.topic('greetings', key_type=str, value_type=EEGReading)
+my_topic = app.topic('greetings', key_type=str, value_type=EegReading)
 
-order_count_by_account = app.Table('order_count', default=int)
-
-table_counts = app.Table('mycount', default=int, key_type=str, value_type=int).tumbling(5.0, expires=5.0)
-table_sum = app.Table('mysum', default=float, key_type=str, value_type=float).tumbling(5.0, expires=5.0)
+table_counts = app.Table('mycount', default=int).tumbling(5.0, expires=5.0)
+table_sum = app.Table('mysum', default=float).tumbling(5.0, expires=5.0)
 
 @app.agent(my_topic)
-async def count_readings(readings: faust.Stream[EEGReading]) -> None:
-    async for reading in readings.group_by(EEGReading.channel):
+async def count_readings(readings):
+    async for reading in readings.group_by(EegReading.channel):
         print(f"Received message with timestamp: {reading.time}")
         table_counts[reading.channel] += 1
         table_sum[reading.channel] += reading.potential
 
 @app.timer(2.0)
 async def report_every_other_second():
-    print(f"   *** {datetime.now()}: we have the following state in table_counts:")
+    print(f"   --- {datetime.now()}: we have the following state in table_counts:")
     print(table_counts)
-    print("   *** and in table_sum:")
+    print("   --- and in table_sum:")
     print(table_sum)
 
 
