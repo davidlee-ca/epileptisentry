608e0258bd2d6fb6ba889da7d0165646ea507338
David Lee <david@sugarcreek.xyz>
2019-06-24 14:43:30 -0400

    Created signal generation using Faust

--- src/faust/process-eeg-signals.py
+++ src/faust/process-eeg-signals.py
@@ -1,26 +1,19 @@
 import faust
-import pywt
-import numpy as np
-import entropy
-from datetime import datetime
 
+
+class EegKey(faust.Record, serialize='json'):
+    ptid: str
+    ch: str
 
 
 class EegReading(faust.Record, serializer='json'):
     time: float
     potential: float
-    channel: str
 
 
-app = faust.App(
-    'helloworld-by-country',
-    broker='kafka://ip-10-0-0-4.ec2.internal:9092'
-)
-
-my_topic = app.topic('greetings', key_type=str, value_type=EegReading)
-
-table_counts = app.Table('mycount', default=int).tumbling(5.0, expires=5.0)
-table_sum = app.Table('mysum', default=float).tumbling(5.0, expires=5.0)
+app = faust.App('process-eeg', broker='kafka://ip-10-0-0-4.ec2.internal:9092')
+source_topic = app.topic('eeg-signals', key_type=EegKey, value_type=EegReading)
+reading_table = app.Table()
 
 
 # from StackExchange -- generate surrogate series!
@@ -29,7 +22,7 @@ def generate_surrogate_series(ts):  # time-series is an array
     random_phases = np.exp(np.random.uniform(0,np.pi,len(ts)//2+1)*1.0j)
     ts_fourier_new = ts_fourier*random_phases
     new_ts = np.fft.irfft(ts_fourier_new)
-    return new_ts
+    return new_ts.tolist()
 
 
 def get_delta_apen(ts): # time-series is an array
@@ -46,19 +39,45 @@ def get_delta_apen(ts): # time-series is an array
     return delta_ApEns
 
 
-@app.agent(my_topic)
+@app.task()
+async def determine_seizure(stream):
+    async for values in stream.take(4096, within=16):
+
+        time_series = [(x.time, x.potential) for x in values]
+        time_series = sorted([time_series], key=lambda x: x[0]) # sort the time-series by instrument timestamp
+        delta_apen = get_delta_apen(time_series)
+
+        print(f'RECEIVED {len(values)} samples with a delta_apen of {delta_apen}')
+
+
+async def create_hopping_windows()
+
+
+@app.agent(source_topic )
 async def count_readings(readings):
-    async for reading in readings.group_by(reading.channel):
-        print(f"Received message with timestamp: {reading.time}")
-        table_counts[reading.channel] += 1
-        table_sum[reading.channel] += reading.potential
+    async for pt_waveset in readings.group_by(EegKey.ptid):
+
+
+
 
 @app.timer(2.0)
-async def report_every_other_second():
-    print(f"   --- {datetime.now()}: we have the following state in table_counts:")
-    print(table_counts)
-    print("   --- and in table_sum:")
-    print(table_sum)
+async def start_rolling_windows():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
