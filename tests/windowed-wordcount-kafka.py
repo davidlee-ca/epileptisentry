37fe2a5f2c864aca8ddedad95a851eb5382781c7
David Lee <david@sugarcreek.xyz>
2019-06-25 06:28:10 -0400

    Added json parsing for key and value

--- tests/windowed-wordcount-kafka.py
+++ tests/windowed-wordcount-kafka.py
@@ -1,74 +1,20 @@
-# https://github.com/apache/spark/blob/master/examples/src/main/python/sql/streaming/structured_kafka_wordcount.py
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-"""
- Consumes messages from one or more topics in Kafka and does wordcount.
- Usage: structured_kafka_wordcount.py <bootstrap-servers> <subscribe-type> <topics>
-   <bootstrap-servers> The Kafka "bootstrap.servers" configuration. A
-   comma-separated list of host:port.
-   <subscribe-type> There are three kinds of type, i.e. 'assign', 'subscribe',
-   'subscribePattern'.
-   |- <assign> Specific TopicPartitions to consume. Json string
-   |  {"topicA":[0,1],"topicB":[2,4]}.
-   |- <subscribe> The topic list to subscribe. A comma-separated list of
-   |  topics.
-   |- <subscribePattern> The pattern used to subscribe to topic(s).
-   |  Java regex string.
-   |- Only one of "assign, "subscribe" or "subscribePattern" options can be
-   |  specified for Kafka source.
-   <topics> Different value format depends on the value of 'subscribe-type'.
- Run the example
-    `$ bin/spark-submit examples/src/main/python/sql/streaming/structured_kafka_wordcount.py \
-    host1:port1,host2:port2 subscribe topic1,topic2`
-"""
-from __future__ import print_function
-
-import sys
-
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
-
-#from pyspark.sql import SparkSession
-#from pyspark.sql.functions import explode
-#from pyspark.sql.functions import split
+from json import loads
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("""
-        Usage: structured_kafka_wordcount.py <bootstrap-servers> <subscribe-type> <topics>
-        """, file=sys.stderr)
-        sys.exit(-1)
 
-    bootstrapServers = sys.argv[1]
-    subscribeType = sys.argv[2]
-    topics = sys.argv[3]
-
-# sarf
-
-    sc = SparkContext(appName="sparkStreamConsumerFromKafkaTest").getOrCreate()
+    sc = SparkContext(appName="SparkStreamConsumerFromKafka").getOrCreate()
     sc.setLogLevel("WARN")
     ssc = StreamingContext(sc, 5)
 
-#   input_from_topic = KafkaUtils.createDirectStream(ssc, [topics], {"metadata.broker.list": bootstrapServers})
-    input_from_topic = KafkaUtils.createStream(ssc, "10.0.0.13:2181", "foo", {topics: 1})
+    input_from_topic = KafkaUtils.createStream(ssc, "10.0.0.13:2181", "sparkApplication", {"words": 1})
+    # input_from_topic = KafkaUtils.createStream(ssc, "10.0.0.13:2181", "sparkApplication", {"eeg-signal": 1})
+    # brings each message into (k, v)
+    # input_from_topic.pprint()
 
-
+    parsed_input = input_from_topic.map(lambda k, v: (loads(k), loads(v)))
     input_from_topic.pprint()
 
     ssc.start()
