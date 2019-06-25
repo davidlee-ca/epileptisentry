43181c9bfc10bfcc1114401d48bf437aa3a335ca
Ubuntu <ubuntu@ip-10-0-0-14.ec2.internal>
2019-06-25 12:28:46 +0000

    Added sliding window

--- src/spark/process-single-channel.py
+++ src/spark/process-single-channel.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 
     sc = SparkContext(appName="SparkStreamConsumerFromKafka").getOrCreate()
     sc.setLogLevel("WARN")
-    ssc = StreamingContext(sc, 8)
+    ssc = StreamingContext(sc, 2)
 
     raw_topic = KafkaUtils.createStream(ssc, "10.0.0.13:2181", "sparkApplication", {"eeg-signal": 1})
     parsed_input = raw_topic.map(lambda v: (loads(v[0]), loads(v[1]))) # expect JSON serialization
@@ -18,11 +18,15 @@ if __name__ == "__main__":
     parsed_input.count()
     parsed_input.pprint()
 
-
     # for MVP - filter a single
     filtered_input = parsed_input.filter(lambda x: (x[0] == "chb01") and (x[1] == "FP1-F3"))
     filtered_input.count()
     filtered_input.pprint()
+
+    # apparently it's easy to do windowing... let's see
+    filtered_windowed_input = filtered_input.window(8, 2)
+    filtered_windowed_input.pprint()
+
 
     ssc.start()
     ssc.awaitTermination()
