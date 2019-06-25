f0a62f59cc76b58fac45962a1f02cbb501f8a1d4
Ubuntu <ubuntu@ip-10-0-0-14.ec2.internal>
2019-06-25 19:50:51 +0000

    Patched up timescaledb insertion test

--- tests/test-insert-to-timescaledb.py
+++ tests/test-insert-to-timescaledb.py
@@ -3,8 +3,16 @@ import psycopg2
 from pyspark import SparkContext
 from pyspark.sql import *
 from time import time
-from os import environ
+from datetime import datetime as dt
+import os
 
+
+
+postgres_url = "jdbc:postgresql://ec2-3-215-187-200.compute-1.amazonaws.com:5432/speegs"
+properties = {
+    "user": os.environ['POSTGRES_USER'],
+    "password": os.environ['POSTGRES_PASSWORD']
+}
 
 if __name__ == "__main__":
 
@@ -12,20 +20,14 @@ if __name__ == "__main__":
     sc.setLogLevel("WARN")
     spark = SparkSession(sc)
 
-    rawdata1 = Row(subject='chb01', channel='FP1-F3', timestamp=time()+1, v=3.141592)
-    rawdata2 = Row(subject='chb01', channel='FP1-F3', timestamp=time()+2, v=3.141592)
-    rawdata3 = Row(subject='chb01', channel='FP1-F3', timestamp=time()+3, v=3.141592)
+    rawdata1 = Row(subject_id='chb01', channel='FP1-F3', instrument_time=dt.fromtimestamp(time()+1), reading=3.141592)
+    rawdata2 = Row(subject_id='chb01', channel='FP1-F3', instrument_time=dt.fromtimestamp(time()+2), reading=3.141592)
+    rawdata3 = Row(subject_id='chb01', channel='FP1-F3', instrument_time=dt.fromtimestamp(time()+3), reading=3.141592)
 
     df_raw = spark.createDataFrame([rawdata1, rawdata2, rawdata3])
     df_raw.show()
 
-    postgres_url = "jdbc:postgresql://ec2-3-215-187-200.compute-1.amazonaws.com:5432/speegs"
-    properties = {
-        "user": environ(['POSTGRES_USER']),
-        "password": environ(['POSTGRES_PASSSWORD'])
-    }
-
-    df_raw.write.jdbc(url=url, table="eeg_signal", properties=properties)
+    df_raw.write.jdbc(url=postgres_url, table="eeg_data", mode="append", properties=properties)
 
 """
 
