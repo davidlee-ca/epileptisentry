8d0da11471c24e38aae040cd5078f686968e170a
Ubuntu <ubuntu@ip-10-0-0-14.ec2.internal>
2019-06-25 10:56:23 +0000

    Added ability to parse into key

--- tests/windowed-wordcount-kafka.py
+++ tests/windowed-wordcount-kafka.py
@@ -12,10 +12,13 @@ if __name__ == "__main__":
     input_from_topic = KafkaUtils.createStream(ssc, "10.0.0.13:2181", "sparkApplication", {"words": 1})
     # input_from_topic = KafkaUtils.createStream(ssc, "10.0.0.13:2181", "sparkApplication", {"eeg-signal": 1})
     # brings each message into (k, v)
-    # input_from_topic.pprint()
-
-    parsed_input = input_from_topic.map(lambda k, v: (loads(k), loads(v)))
     input_from_topic.pprint()
+
+    parsed_input = input_from_topic.map(lambda v: (loads(v[0]), loads(v[1])))
+    parsed_input.pprint()
+
+    parsed_input = parsed_input.map(lambda v: (v[0], type(v[0]), v[0]['a']))
+    parsed_input.pprint()
 
     ssc.start()
     ssc.awaitTermination()
