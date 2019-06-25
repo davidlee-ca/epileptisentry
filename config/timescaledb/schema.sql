4eb06c9d203550c673691d1052c112778ad2096e
David Lee <david@sugarcreek.xyz>
2019-06-25 14:40:34 -0400

    Created a simplified script to produce Kafka signals

--- config/timescaledb/schema.sql
+++ config/timescaledb/schema.sql
@@ -14,13 +14,17 @@ CREATE INDEX ON eeg_data (subject_id, instrument_time DESC);
 CREATE INDEX ON eeg_data (instrument_time DESC, subject_id);
 CREATE INDEX ON eeg_data (channel, instrument_time DESC);
 
-/*
+
 DROP TABLE IF EXISTS "eeg_analysis";
 CREATE TABLE "eeg_analysis"(
     instrument_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
 	subject_id TEXT,
 	channel TEXT,
-	delta_apen NUMERIC,
+	seizure_metric NUMERIC,
+	num_datapoints NUMERIC,
 );
+
 SELECT create_hypertable('eeg_analysis', 'instrument_time');
-*/
+CREATE INDEX on eeg_analysis (subject_id, instrument_time DESC);
+CREATE INDEX on eeg_analysis (instrument_time DESC, subject_id);
+CREATE INDEX on eeg_analysis (instrument_time DESC, seizure_metric);
---
+++ src/kafka/produce-signals.py
@@ -0,0 +1,49 @@
+from confluent_kafka import Producer
+from time import time, sleep
+import boto3
+import sys
+
+
+broker = "10.0.0.8:9092, 10.0.0.11:9092, 10.0.0.4:9092"
+topic = "eeg-data"
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        sys.stderr.write('Usage: %s <subject_id> <replay file number>\n' % sys.argv[0])
+        sys.exit(1)
+
+    subject_id = sys.argv[1]
+    replay_file_number = sys.argv[2]
+
+    # Producer configuration
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+    conf = {'bootstrap.servers': broker,
+            'group.id': 'eeg-player'}
+
+    # Create Producer instance
+    p = Producer(**conf)
+
+    # Open the EEG file from the S3 bucket
+    mybucket = 'speegs-source-chbmit'
+    mykey = f"{subject_id}/{subject_id}_{replay_file_number}.csv"
+
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket=mybucket, Key=mykey)
+
+    start_time = time()
+    channels = (
+        'FP1-F7', 'F7-T7', 'T7-P7', 'P7-O1', 'FP1-F3', 'F3-C3', 'C3-P3', 'P3-O1', 'FP2-F4', 'F4-C4', 'C4-P4', 'P4-O2',
+        'FP2-F8', 'F8-T8', 'T8-P8', 'P8-O2', 'FZ-CZ', 'CZ-PZ', 'P7-T7', 'T7-FT9', 'FT9-FT10', 'FT10-T8', 'T8-P8')
+
+    obj._raw_stream.readline() # throw away the first line
+
+    for line in obj._raw_stream.readline():
+        readings = str(mystring.strip()).split(',')
+        for i in range(23):
+            key = '{"subject": "%s", "ch": "%s"' % subject_id, channels[i]
+            value = '{"timestamp": %.6f, "v": %.6f}' % start_time + float(readings[0]), float(readings[i + 1])
+            p.produce(topic, value=value, key=key)
+        sleep(1)  # tunable
+        p.poll(0)
+
+    p.flush()
