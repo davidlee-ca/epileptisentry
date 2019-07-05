c6ca2fec26d5817fe737fb92af6bdac0c396ef98
Ubuntu <ubuntu@ip-10-0-1-22.ec2.internal>
2019-07-05 19:39:01 +0000

    Batched message flushing to enable 6-way EEG signal generation

--- src/kafka/produce-signals.py
+++ src/kafka/produce-signals.py
@@ -4,7 +4,7 @@ import boto3
 import sys
 
 
-broker = "10.0.1.62:9092,10.0.1.24:9092,10.0.1.35:9092,10.0.1.17:9092,10.0.1.39:9092"
+brokers = "10.0.1.62:9092,10.0.1.24:9092,10.0.1.35:9092,10.0.1.17:9092,10.0.1.39:9092"
 
 if __name__ == '__main__':
 
@@ -16,13 +16,14 @@ if __name__ == '__main__':
     replay_file_number = sys.argv[2]
     topic = "eeg-signal"
 
-    frequency = 256
-    delay = 1.0 / frequency * 0.9
+    frequency = 256.0
+    delay = 0.80 / frequency
+    refresh_delay_interval = 5.0
 
 
     # Producer configuration
     # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
-    conf = {'bootstrap.servers': broker,
+    conf = {'bootstrap.servers': brokers,
             'group.id': 'eeg-player'}
 
     # Create Producer instance
@@ -51,16 +52,30 @@ if __name__ == '__main__':
             key = '{"subject": "%s", "ch": "%s"}' % (subject_id, channels[i])
             value = '{"timestamp": %.6f, "v": %.6f}' % (start_time + float(readings[0]), float(readings[i + 1]))
             p.produce(topic, value=value, key=key)
-        sleep(delay)  # tunable -- 2ms worked well for 4, see if 1ms sleep will reduce the deviation
-        p.flush()
 
+        sleep(delay)
         count += 1
 
-        if count == 1280:
+        if count % 5 == 0:  # batch 8 frames before sending to Kafka for efficiency
+            p.flush()
+
+        if count == (refresh_delay_interval * frequency):  # adjust the sleeping interval
+
             new_heartbeat = time()
             duration = new_heartbeat - heartbeat
-            deviation = (5.0 - duration) * 1000
-            delay = delay + deviation / 5000 / 256.0 * 0.5  # 0.5 is the dampening factor
+            deviation = (refresh_delay_interval - duration) * 1000
+
+            try:
+                delay = delay + deviation / 5000 / 256.0 * 0.5  # 0.5 = dampening factor
+                if delay < 0:
+                    raise ValueError
+            except ValueError:
+                delay = 0
+                print("WARNING: NEW DELAY TIME INTERVAL WAS A NEGATIVE NUMBER. Setting to 0..")
+
             print(f"5-second check in for {subject_id}. Deviation: {deviation:.2f} ms, new delay: {delay * 1000:.2f} ms.")
             count = 0
             heartbeat = new_heartbeat
+
+    p.flush()  # flush the remaining signal before termination
+
--- src/kafka/start_producers2.sh
+++
@@ -1,4 +0,0 @@
-python eeg-single-topic.py chb01 03 &
-python eeg-single-topic.py chb02 01 &
-python eeg-single-topic.py chb03 01 &
-python eeg-single-topic.py chb04 28 &
