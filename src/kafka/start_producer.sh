726616eacf2d236b808e0ae39266391b407a0ce6
Ubuntu <ubuntu@ip-10-0-1-22.ec2.internal>
2019-07-05 20:34:36 +0000

    Added subjects 7-12

---
+++ src/kafka/start_producer.sh
@@ -0,0 +1,7 @@
+python produce-signals.py chb01 03 &
+python produce-signals.py chb02 01 &
+python produce-signals.py chb03 01 &
+python produce-signals.py chb04 28 &
+python produce-signals.py chb05 06 &
+python produce-signals.py chb06 13 &
+
