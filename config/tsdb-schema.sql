2f6da6fa10fb4c4984dcb53068ef1ad20294c588
David Lee <david@sugarcreek.xyz>
2019-07-05 13:51:14 -0400

    Removed the schema for storing raw data

--- config/tsdb-schema.sql
+++ config/tsdb-schema.sql
@@ -1,30 +1,23 @@
-DROP EXTENSION IF EXISTS timescaledb;
-CREATE EXTENSION IF NOT EXISTS timescaledb;
-DROP TABLE IF EXISTS "eeg_data";
+/*
 
-CREATE TABLE IF NOT EXISTS "eeg_data"(
-    instrument_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    subject_id TEXT,
-    channel TEXT,
-    reading REAL
-);
+TimescaleDB schema to capture the analyzed data.
+To install TimescaleDB, follow the instructions on https://docs.timescale.com/v1.3/getting-started/installation.
+To deploy this
 
-SELECT create_hypertable('eeg_data', 'instrument_time');
-CREATE INDEX ON eeg_data (subject_id, instrument_time DESC);
-CREATE INDEX ON eeg_data (instrument_time DESC, subject_id);
-CREATE INDEX ON eeg_data (channel, instrument_time DESC);
+sudo psql -U postgres -d [database-name] -a -f tsdb-schema.sql
 
+*/
 
 DROP TABLE IF EXISTS "eeg_analysis";
 CREATE TABLE "eeg_analysis"(
-    instrument_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-	subject_id TEXT,
-	channel TEXT,
-	seizure_metric NUMERIC,
-	num_datapoints NUMERIC,
+    instr_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+	subject_id TEXT NOT NULL,
+	channel TEXT NOT NULL,
+	seizure_metric REAL,
+	num_datapoints INTEGER,
 );
 
-SELECT create_hypertable('eeg_analysis', 'instrument_time');
-CREATE INDEX on eeg_analysis (subject_id, instrument_time DESC);
-CREATE INDEX on eeg_analysis (instrument_time DESC, subject_id);
-CREATE INDEX on eeg_analysis (instrument_time DESC, seizure_metric);
+SELECT create_hypertable('eeg_analysis', 'instr_time');
+CREATE INDEX on eeg_analysis (subject_id, instr_time DESC);
+CREATE INDEX on eeg_analysis (instr_time DESC, subject_id);
+CREATE INDEX on eeg_analysis (instr_time DESC, seizure_metric);
