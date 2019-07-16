f9ede218c115448f69c49033920a6e813004d91f
Ubuntu <ubuntu@ip-10-0-1-38.ec2.internal>
2019-07-16 15:50:13 +0000

    Minor updates on schema

--- config/tsdb-schema.sql
+++ config/tsdb-schema.sql
@@ -10,15 +10,14 @@ sudo psql -U postgres -d [database-name] -a -f tsdb-schema.sql
 
 DROP TABLE IF EXISTS "eeg_analysis";
 CREATE TABLE "eeg_analysis"(
-    instr_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+	instr_time TIMESTAMP WITHOUT TIME ZONE NOT NULL,
 	ingest_time TIMESTAMP WITHOUT TIME ZONE,
 	subject_id TEXT NOT NULL,
 	channel TEXT NOT NULL,
 	abnormality_indicator REAL,
-	num_datapoints INTEGER,
+	num_datapoints INTEGER
 );
 
 SELECT create_hypertable('eeg_analysis', 'instr_time');
 CREATE INDEX on eeg_analysis (subject_id, instr_time DESC);
-CREATE INDEX on eeg_analysis (instr_time DESC, subject_id);
-CREATE INDEX on eeg_analysis (instr_time DESC, seizure_metric);
+CREATE INDEX on eeg_analysis (channel, instr_time DESC);
